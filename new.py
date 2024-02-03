e2626c2c11f0c9136a20b3fbe96f6cf49bd52418
tct123 <42028373+tct123@users.noreply.github.com>
2024-02-04 00:17:42 +0100

    Initial commit

---
+++ new.py
@@ -0,0 +1,14 @@
+def createlist():
+    with open("test.txt", "r") as f:
+        mylist = f.read()
+    mylist = mylist.splitlines()
+    # print(mylist)
+    secondlist = []
+    # for eintrag in mylist:
+    #    secondlist.append(eintrag)
+    # print(secondlist)
+    for entry in mylist:
+        print(f'ft.dropdown.Option("{entry}"),')
+
+
+createlist()
